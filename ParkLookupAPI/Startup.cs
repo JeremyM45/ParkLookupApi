459e94a8d1edaae4a67e37799bf826eb44c0749e
JeremyM45 <jeremyvs1945@gmail.com>
2022-06-10 08:32:40 -0700

    fix errors in startup file

--- ParkLookupAPI/Startup.cs
+++ ParkLookupAPI/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ParkLookupAPI.Models;
 
 namespace ParkLookupAPI
 {
@@ -27,7 +29,7 @@ namespace ParkLookupAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<ParkLookupContext>(opt =>
+            services.AddDbContext<ParkLookupAPIContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
         }
